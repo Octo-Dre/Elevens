57fa3f68d71e7df160aa1fbc3c15ff4b53a1c7b7
bhayes724 <bhayes724@gmail.com>
2015-04-17 14:16:23 -0700

    activity 4 kinda

--- Activity04/DeckTester.java
+++ Activity04/DeckTester.java
@@ -1,58 +1,88 @@
 package Activity04;
 
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * This is a class that tests the Deck class.
  */
 public class DeckTester {
 
-	/**
-	 * The main method in this class checks the Deck operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		String[] ranks = {"jack", "queen", "king"};
-		String[] suits = {"blue", "red"};
-		int[] pointValues = {11, 12, 13};
-		Deck d = new Deck(ranks, suits, pointValues);
+    /**
+     * The main method in this class checks the Deck operations for consistency.
+     *  @param args is not used.
+     */
+    public static void main(String[] args) {
+        String[] ranks = {"jack", "queen", "king"};
+        String[] suits = {"blue", "red"};
+        int[] pointValues = {11, 12, 13};
+        Deck d = new Deck(ranks, suits, pointValues);
 
-		System.out.println("**** Original Deck Methods ****");
-		System.out.println("  toString:\n" + d.toString());
-		System.out.println("  isEmpty: " + d.isEmpty());
-		System.out.println("  size: " + d.size());
-		System.out.println();
-		System.out.println();
+        System.out.println("**** Original Deck Methods ****");
+        System.out.println("  toString:\n" + d.toString());
+        System.out.println("  isEmpty: " + d.isEmpty());
+        System.out.println("  size: " + d.size());
+        System.out.println();
+        System.out.println();
 
-		System.out.println("**** Deal a Card ****");
-		System.out.println("  deal: " + d.deal());
-		System.out.println();
-		System.out.println();
+        System.out.println("**** Deal a Card ****");
+        System.out.println("  deal: " + d.deal());
+        System.out.println();
+        System.out.println();
 
-		System.out.println("**** Deck Methods After 1 Card Dealt ****");
-		System.out.println("  toString:\n" + d.toString());
-		System.out.println("  isEmpty: " + d.isEmpty());
-		System.out.println("  size: " + d.size());
-		System.out.println();
-		System.out.println();
+        System.out.println("**** Deck Methods After 1 Card Dealt ****");
+        System.out.println("  toString:\n" + d.toString());
+        System.out.println("  isEmpty: " + d.isEmpty());
+        System.out.println("  size: " + d.size());
+        System.out.println();
+        System.out.println();
 
-		System.out.println("**** Deal Remaining 5 Cards ****");
-		for (int i = 0; i < 5; i++) {
-			System.out.println("  deal: " + d.deal());
-		}
-		System.out.println();
-		System.out.println();
+        System.out.println("**** Deal Remaining 5 Cards ****");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("  deal: " + d.deal());
+        }
+        System.out.println();
+        System.out.println();
 
-		System.out.println("**** Deck Methods After All Cards Dealt ****");
-		System.out.println("  toString:\n" + d.toString());
-		System.out.println("  isEmpty: " + d.isEmpty());
-		System.out.println("  size: " + d.size());
-		System.out.println();
-		System.out.println();
+        System.out.println("**** Deck Methods After All Cards Dealt ****");
+        System.out.println("  toString:\n" + d.toString());
+        System.out.println("  isEmpty: " + d.isEmpty());
+        System.out.println("  size: " + d.size());
+        System.out.println();
+        System.out.println();
 
-		System.out.println("**** Deal a Card From Empty Deck ****");
-		System.out.println("  deal: " + d.deal());
-		System.out.println();
-		System.out.println();
+        System.out.println("**** Deal a Card From Empty Deck ****");
+        System.out.println("  deal: " + d.deal());
+        System.out.println();
+        System.out.println();
 
-		/* *** TO BE COMPLETED IN ACTIVITY 4 *** */
-	}
+        /* *** TO BE COMPLETED IN ACTIVITY 4 *** */
+    }
+    
+    /**
+     * Compares two ArrayLists to see if they are permutations of each other
+     * @param a
+     * @param b
+     * @return a boolean to indicate whether a and b are permutations
+     */
+    /*public boolean arePermutations(Deck a, Deck b){
+        if(a.size() != b.size())
+            return false;
+        else{
+            for(Card aCard : a){
+                for(Card bCard : b){
+                    if(aCard.matches(bCard)){
+                        a.remove(aCard);
+                        b.remove(bCard);
+                        break;
+                    }
+                }
+            }
+            if(a.size() == 0 && b.size() == 0)
+                return true;
+            else
+                return false;
+        }
+    }*/
+    
 }
---
+++ Activity04/DeckTesterTest.java
@@ -0,0 +1,55 @@
+package Activity04;
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class DeckTesterTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class DeckTesterTest
+{
+    /**
+     * Default constructor for test class DeckTesterTest
+     */
+    public DeckTesterTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+    /*
+    @Test
+    public void testArePermutations()
+    {
+        String[] ranks = {"jack", "queen", "king"};
+        String[] suits = {"blue", "red"};
+        int[] pointValues = {11, 12, 13};
+        Deck d = new Deck(ranks, suits, pointValues);
+        Deck e = new Deck(ranks, suits, pointValues);
+        assertEquals(true, DeckTester.arePermutations(d, e));
+    } */
+}
+
